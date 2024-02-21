966b75712e4d46d297b3dabce2892f3d1dd487f0
Kavin Abregana <abreganakavin@gmail.com>
2024-02-21 15:16:01 +0800

    header

--- _v4/porfolio-v4/src/App.jsx
+++ _v4/porfolio-v4/src/App.jsx
@@ -1,7 +1,20 @@
-// import AppLayout from "./ui/AppLayout";
+import MainNav from './ui/MainNav';
+import Logo from './ui/Logo';
+import Button from './ui/Button';
+import DarkModeToggle from './ui/DarkModeToggle';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 function App() {
-  return <h1>Hello</h1>;
+    return (
+        <DarkModeProvider>
+            <header>
+                <Logo />
+                <MainNav />
+                <Button>Download CV</Button>
+                <DarkModeToggle />
+            </header>
+        </DarkModeProvider>
+    );
 }
 
 export default App;
--- _v4/porfolio-v4/src/ui/Logo.jsx
+++ _v4/porfolio-v4/src/ui/Logo.jsx
@@ -1,23 +1,24 @@
-import styled from "styled-components";
-import { useDarkMode } from "../context/DarkModeContext";
+import styled from 'styled-components';
+// import { useDarkMode } from '../context/DarkModeContext';
 
 const StyledLogo = styled.div`
-  text-align: center;
+    text-align: center;
 `;
 
-const Img = styled.img`
-  height: 9.6rem;
-  width: auto;
-`;
+// const Img = styled.img`
+//     height: 9.6rem;
+//     width: auto;
+// `;
 
 function Logo() {
-  const { isDarkMode } = useDarkMode();
-  const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
-  return (
-    <StyledLogo>
-      <Img src={src} alt="Logo" />
-    </StyledLogo>
-  );
+    // const { isDarkMode } = useDarkMode();
+    // const src = isDarkMode ? '/logo-dark.png' : '/logo-light.png';
+    return (
+        <StyledLogo>
+            {/* <Img src="/logo-light.png" alt="Logo" /> */}
+            <h2>Kavin Abregana</h2>
+        </StyledLogo>
+    );
 }
 
 export default Logo;
